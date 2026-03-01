145290d1c4ce305d2e61a42dc716d56b8a0aff5d
Anilvarikala <nadeemahmedmohammad7@gmail.com>
2026-03-01 21:00:33 +0530

    fix issue

--- buzzpaye-ml/app.py
+++ buzzpaye-ml/app.py
@@ -275,4 +275,5 @@ def recommend_influencers(campaign_id):
 # ------------------ Run Server ------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
